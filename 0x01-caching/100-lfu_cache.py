c7b2d3e8077dbf3af3bc06dd8ccd1304d87273e0
Collins <ochiengcollinsoduor2@gmail.com>
2024-05-22 17:10:45 +0300

    LFU cache system update

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""LFU caching"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFU cache system """
+
+    def __init__(self):
+        """ Initialize LFU cache """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.queue.remove(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    discard = self.queue.pop(0)
+                    del self.cache_data[discard]
+                    print("DISCARD:", discard)
+                self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
